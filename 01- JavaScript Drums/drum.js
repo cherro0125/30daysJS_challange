e6ee45e98e6dc029e9ee027fc2386b0223f4bfbb
Cherro <kaliszewski.dominik@gmail.com>
2018-07-03 23:40:40 +0200

    Complete first challange

---
+++ 01- JavaScript Drums/drum.js
@@ -0,0 +1,23 @@
+
+function playDrum(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add("playing");
+}
+
+function removeClassEffect(e) {
+    if (e.propertyName !== "transform") return;
+    this.classList.remove("playing");
+}
+
+
+const keys = document.querySelectorAll(".key");
+
+keys.forEach(key => {
+    key.addEventListener("transitionend", removeClassEffect);
+});
+
+window.addEventListener("keydown", playDrum);
