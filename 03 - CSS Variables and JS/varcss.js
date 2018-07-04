ff500bd05ba88619ffdf80da5e7504d11b0a008a
Cherro <kaliszewski.dominik@gmail.com>
2018-07-04 21:51:06 +0200

    Complete third challange

---
+++ 03 - CSS Variables and JS/varcss.js
@@ -0,0 +1,13 @@
+const inputs = document.querySelectorAll(".controls input");
+
+
+
+function changeVars() {
+    const suffix = this.dataset.sizing || "";
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+inputs.forEach((input => {
+    input.addEventListener("change", changeVars);
+    input.addEventListener("mousemove", changeVars);
+}));
